35cc9b2445a4a100f27988119dad584a8894a2df
Bhavana2024 <bhavanag020@gmail.com>
2025-03-18 13:19:49 +0530

    commit new3 18-03

---
+++ PracticePrograms/TwoArrayIsEqualOrNot.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticePrograms
+{
+    public class TwoArrayIsEqualOrNot
+    {
+        public void twoArrayIsEqualOrNot()
+        {
+            int[] a = { 1, 2, 3 };
+            int[] b = { 1, 2, 3};
+            bool isEqual = true;
+            if (a.Length != b.Length)
+            {
+                isEqual = false;
+            }
+            else
+            {
+                for (int i = 0; i < a.Length; i++)
+                {
+                    if (a[i] != b[i])
+                    {
+                        isEqual = false;
+                        break;
+                    }
+                }
+            }
+            if(isEqual)
+            {
+                Console.WriteLine("Array is Same");
+            }
+            else
+            {
+                Console.WriteLine("Array is not same");
+            }
+        }
+        
+    }
+}
---
+++ PracticePrograms/TwoStringsAreEqualOrNot.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticePrograms
+{
+    public class TwoStringsAreEqualOrNot
+    {
+        public void stringEqualOrNot()
+        {
+            string str1 = "hello";
+            string str2 = "olleh";
+            bool isEqual = true;
+            if (str1.Length != str2.Length)
+            {
+                isEqual = false;
+            }
+            else
+            {
+                for (int i = 0; i < str1.Length; i++)
+                {
+                    if (str1[i]!= str2[i])
+                    {
+                        isEqual = false;
+                        break;
+                    }
+                }
+            }
+
+            if (isEqual == true) {
+                Console.WriteLine("string is equal");
+            }
+            else
+            {
+                Console.WriteLine("string is not equal");
+            }
+        }
+    }
+}
