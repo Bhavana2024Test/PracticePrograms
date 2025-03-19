2d006d7cf93ebdd375166f7a524a10fb0bd6896c
Bhavana2024 <bhavanag020@gmail.com>
2025-03-19 13:07:00 +0530

    commit new21 19-03

---
+++ PracticePrograms/NumberIsPalidrome.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticePrograms
+{
+    public class NumberIsPalidrome
+    {
+        public void PalindromeNumber()
+        {
+            int a = 121;
+            int rev = 0;
+            int num = a;
+            while(a!=0)
+            {
+                int rem = a % 10;
+                rev = rev * 10 + rem;
+                a = a / 10;
+
+            }
+            if(num==rev)
+            {
+                Console.WriteLine("palindrome");
+            }
+            else
+            {
+                Console.WriteLine("NOt a Palindrome");
+            }
+        }
+    }
+}
